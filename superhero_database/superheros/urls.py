93e7073718a776b4b0f3cd01d6306093b8ebc2a0
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-07 14:59:09 -0400

    added 6 superheros to django admin list,currently adding detail search for users but having issues

--- superhero_database/superheros/urls.py
+++ superhero_database/superheros/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'superheros'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:superhero_id>/', views.detail, name='details')
 ]
--- superhero_database/superheros/views.py
+++ superhero_database/superheros/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
+# from .urls import detail
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello")
+    all_superheros = Superhero.objects.all()
+    context = {
+        'all_superheros': all_superheros
+    }
+    return render(request, 'superheros/index.html', context)
+
+# def detail(request, superhero_id)
