a385aa250eeecbbf697799972e546f261455432d
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-07 12:45:28 -0400

    forgot to create repo at the beginning

---
+++ superhero_database/superheros/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+# superhero’s name, alter ego, primary superhero ability, secondary superhero ability, and catchphrase.
+class Superhero(models.Model):
+    name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    ability = models.CharField(max_length=50)
+    secondary_superhero_ability = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
