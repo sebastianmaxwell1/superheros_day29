3a60ea70fd3a3c611a31d718b5156ae7329a6919
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-07 21:47:41 -0400

    adding detail function, still sucks

--- superhero_database/superheros/urls.py
+++ superhero_database/superheros/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'superheros'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:superhero_id>/', views.detail, name='detail')
+    path('<int:superhero_id>/', views.detail, name='detail'),
 ]
--- superhero_database/superheros/views.py
+++ superhero_database/superheros/views.py
@@ -12,5 +12,14 @@ def index(request):
 
 
 def detail(request, superhero_id):
-    chose_superhero = Superhero.objects.get(pk=superhero_id)
-    return render(request, 'superheros/index.html', chose_superhero)
+    pick_superhero = Superhero.objects.get(pk=superhero_id)
+    context = {
+        'pick_superhero': pick_superhero
+    }
+    return render(request, 'superheros/detail.html', context)
+
+
+
+
+
+
