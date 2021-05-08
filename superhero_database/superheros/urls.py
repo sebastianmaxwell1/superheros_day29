3529dec41d530f90653514a623eae049251822ef
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-05-07 20:18:08 -0400

    created html templates

--- superhero_database/superheros/urls.py
+++ superhero_database/superheros/urls.py
@@ -5,4 +5,7 @@ app_name = 'superheros'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create_superhero'),
+    path('edit/<int:superhero_id>', views.edit, name='edit_superhero'),
+    path('delete/<int:superhero_id>', views.delete, name='delete_superhero')
 ]
--- superhero_database/superheros/views.py
+++ superhero_database/superheros/views.py
@@ -12,14 +12,52 @@ def index(request):
 
 
 def detail(request, superhero_id):
-    pick_superhero = Superhero.objects.get(pk=superhero_id)
+    pick_superhero = Superhero.objects.get(Superhero, pk=superhero_id)
     context = {
         'pick_superhero': pick_superhero
     }
     return render(request, 'superheros/detail.html', context)
 
 
+def create(request, superhero_id):
+    form = SuperherForm(request.POST or None, request.FILES or None)
+
+    context = {
+        'form': form
+    }
+
+    if form.is_valid():
+        form.save()
+        return redirect('superheros:index')
+    else:
+        return render(request, 'superheros/create.html', context)
 
 
+def edit(request, superhero_id):
+    superhero = get_objects_or_404(Superhero, pk=superhero_id)
+
+    form = SuperheroForm(request.POST or None, request.FILES or None, instance=superhero)
+
+    if form.is_valid():
+        form.save()
+        return redirect('superheros:index')
+
+    context = {
+        'form': form,
+        'superhero image': superhero
+    }
+
+    return render(request, 'superheros/edit.html', context)
 
 
+def delete(request, superhero_id):
+    superhero = get_objects_or_404(Superhero, pk=superhero_id)
+    if request.method == 'POST':
+        superhero.delete()
+        return redirect('superheros:index')
+
+    context = {
+        'superhero': superhero
+    }
+
+    return render(request, 'superheros/delete.html', context)
